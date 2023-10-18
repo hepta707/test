5119dd7923a955c74218f28994c323f1db7ab821
hepta707 <148298434+hepta707@users.noreply.github.com>
2023-10-18 10:42:53 +0900

    Add files via upload

---
+++ time - コピー (4).py
@@ -0,0 +1,37 @@
+import time
+import subprocess
+
+while True:
+    # 1時間待つ
+    time.sleep(60)
+    
+    # Pythonスクリプトを実行する
+    subprocess.run(['python', 'tw-seeking_alpha.py'])
+
+while True:
+    # 1時間待つ
+    time.sleep(70)
+    
+    # Pythonスクリプトを実行する
+    subprocess.run(['python', 'tw-deepl3.py'])
+
+while True:
+    # 1時間待つ
+    time.sleep(80)
+    
+    # Pythonスクリプトを実行する
+    subprocess.run(['python', 'tw-globenewswire.py'])
+
+while True:
+    # 1時間待つ
+    time.sleep(90)
+    
+    # Pythonスクリプトを実行する
+    subprocess.run(['python', 'tw-tdnet_all.py'])
+
+# while True:
+#     # 1時間待つ
+#     time.sleep(60)
+    
+#     # Pythonスクリプトを実行する
+#     subprocess.run(['python', 'tw-seeking_alpha.py'])
